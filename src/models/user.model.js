f32883019ce1a4a3721e1daaa165ae7ecb551368
SANDIPAN NAYEK <sandipannayek09@gmail.com>
2026-04-02 22:31:34 +0530

    add user model with schema definition

---
+++ src/models/user.model.js
@@ -0,0 +1,26 @@
+import mongoose,{Schema} from "mongoose";
+
+const userSchema = new Schema({
+    username:{
+        type:String,
+        unique:true,
+        required:true,
+        lowercase:true,
+        trim:true
+    },
+    email:{
+     type:String,
+     unique:true,
+     required:true,
+     lowercase:true,
+     trim:true
+    },
+    password:{
+     type:String,
+     required:true
+    }
+},{
+    timestamps:true
+})
+
+export const UserModel = mongoose.model("UserModel",userSchema)
