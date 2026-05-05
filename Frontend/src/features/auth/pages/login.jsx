4d0ba6707f2fac39b0894173526dd82850879bb6
SANDIPAN NAYEK <sandipannayek09@gmail.com>
2026-05-05 20:09:27 +0530

    feat: add login page with form styling and button styles

--- Frontend/src/features/auth/pages/login.jsx
+++ Frontend/src/features/auth/pages/login.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import "../auth.form.scss"
 const Login = () => {
   return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
+   <main>
+     <div className="form-container">
+        <h1>Login Page</h1>
+
+        <form >
+
+         <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email" placeholder='Enter email address' />
+         </div>
+
+         <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" placeholder='Enter password' />
+         </div>
+            <button type="submit">Login</button>
+
+        </form>
+     </div>
+   </main>
   )
 }
 
